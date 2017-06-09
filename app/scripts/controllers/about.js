1f18ae98ceb33886fea73b54d4c2a3a8f964aacc
Darius Moore <support@dariusmoore.me>
2017-06-09 00:15:29 -0400

    Yeoman and Angular Installed

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modLab2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the modLab2App
+ */
+angular.module('modLab2App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modLab2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the modLab2App
+ */
+angular.module('modLab2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
